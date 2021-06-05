fc6e40798f4f6e5e3bad5b84f01c88754d668697
ManuLasker <emanuel.afanador@koombea.com>
2021-06-04 22:45:30 -0500

    :rocket:  add new router for user refresh and logout token

--- sqlalchemy_fastapi/src/api/endpoints/__init__.py
+++ sqlalchemy_fastapi/src/api/endpoints/__init__.py
@@ -1,3 +1,9 @@
 from src.api.endpoints.item import ItemRouter, ItemListRouter
-from src.api.endpoints.user import UserRouter, UserRegisterRouter, UserAuthenticationRouter
+from src.api.endpoints.user import (
+    UserRouter,
+    UserRegisterRouter,
+    UserLogoutRouter,
+    UserAuthenticationRouter,
+    UserRefresherRouter,
+)
 from src.api.endpoints.store import StoreRouter, StoreListRouter
