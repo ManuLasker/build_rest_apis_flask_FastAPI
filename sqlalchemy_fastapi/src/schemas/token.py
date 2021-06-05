7c00a3953c5a3b33e8a5f85b70be12c8651d5efb
ManuLasker <emanuel.afanador@koombea.com>
2021-06-04 22:46:45 -0500

    :rocket:  implement schema for token

--- sqlalchemy_fastapi/src/schemas/token.py
+++ sqlalchemy_fastapi/src/schemas/token.py
@@ -1,5 +1,21 @@
-from pydantic import BaseModel
-
+from pydantic import BaseModel, validator
+from datetime import datetime
 class Token(BaseModel):
     access_token: str
     refresh_token: str
+    
+class TokenDecode(BaseModel):
+    fresh: bool
+    iat: datetime
+    jti: str
+    nbf: datetime
+    type: str
+    sub: int
+    exp: datetime
+    is_admin: bool
+    
+    @validator("type", pre=True)
+    def validate_type(cls, type_: str) -> str:
+        if not type_ in ["access", "refresh"]:
+            raise ValueError("type must be one of access or refresh")
+        return type_
