4e8cd4515253fa288c639a97b41a848b2989c8a1
ManuLasker <emanuel.afanador@koombea.com>
2021-06-04 22:44:59 -0500

    :rocket: format document and add new router for refresh jwt and logout to expire jwt

--- sqlalchemy_fastapi/src/api/__init__.py
+++ sqlalchemy_fastapi/src/api/__init__.py
@@ -1,33 +1,34 @@
 from fastapi import APIRouter
-from src.api.endpoints import (ItemRouter, ItemListRouter,
-                               UserRegisterRouter, UserAuthenticationRouter,
-                               UserRouter, StoreListRouter, StoreRouter)
+from src.api.endpoints import (
+    ItemRouter,
+    ItemListRouter,
+    UserRegisterRouter,
+    UserAuthenticationRouter,
+    UserRouter,
+    UserRefresherRouter,
+    UserLogoutRouter,
+    StoreListRouter,
+    StoreRouter,
+)
 
+UserLogoutRouter
 # add api router
 api_router = APIRouter()
 # add item router
-api_router.include_router(ItemRouter.router,
-                          prefix="/item",
-                          tags=["item"])
+api_router.include_router(ItemRouter.router, prefix="/item", tags=["item"])
 # add items router
-api_router.include_router(ItemListRouter.router,
-                          prefix="/items",
-                          tags=["items"])
+api_router.include_router(ItemListRouter.router, prefix="/items", tags=["items"])
 # add user router
-api_router.include_router(UserRouter.router,
-                          prefix="/user",
-                          tags=["user"])
+api_router.include_router(UserRouter.router, prefix="/user", tags=["user"])
 # add store router
-api_router.include_router(StoreRouter.router,
-                          prefix="/store",
-                          tags=["store"])
+api_router.include_router(StoreRouter.router, prefix="/store", tags=["store"])
 # add stores router
-api_router.include_router(StoreListRouter.router,
-                          prefix="/stores",
-                          tags=["stores"])
+api_router.include_router(StoreListRouter.router, prefix="/stores", tags=["stores"])
 # add register user router
-api_router.include_router(UserRegisterRouter.router,
-                          tags=["user"])
+api_router.include_router(UserRegisterRouter.router, tags=["user"])
 # add authentication router
-api_router.include_router(UserAuthenticationRouter.router,
-                          tags=["user"])
+api_router.include_router(UserAuthenticationRouter.router, tags=["user"])
+# add router refresh
+api_router.include_router(UserRefresherRouter.router, tags=["user"])
+# add logout user router
+api_router.include_router(UserLogoutRouter.router, tags=["user"])
